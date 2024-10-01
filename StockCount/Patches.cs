170e6a0d069f5560160f246f368fc95ff2bb0fa7
Speshkitty <sirspeshkitty+github@gmail.com>
2024-10-01 11:34:44 +0100

    Updated for 0.41 Hopefully fixed crashing

--- StockCount/Patches.cs
+++ StockCount/Patches.cs
@@ -2,25 +2,81 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace StockCount
 {
-    [HarmonyPatch(typeof(RestockItemPanelUI), nameof(RestockItemPanelUI.Init))]
+    [HarmonyPatch]
     class Patches
     {
-        public static void Postfix(RestockItemPanelUI __instance, int index)
+        private static Dictionary<EItemType, int> StockAmounts = [];
+
+        [HarmonyPatch(typeof(RestockItemPanelUI), nameof(RestockItemPanelUI.Init))]
+        [HarmonyPostfix]
+        public static void PatchQuantity(RestockItemPanelUI __instance, int index)
         {
-            Plugin.Logger.LogInfo($"Postfix for {__instance.m_ItemNameText.text} ran!");
 
-            RestockData restockData = InventoryBase.GetRestockData(index);
-
-            if (Plugin.StockAmounts.TryGetValue(restockData.itemType, out var amount))
+            Plugin.Log($"Postfix for {__instance.m_ItemNameText.text} ran!");
+            try
             {
-                __instance.m_AmountText.text = $"Stock: {amount}";
+                RestockData restockData = InventoryBase.GetRestockData(index);
+
+                if (StockAmounts.TryGetValue(restockData.itemType, out var amount))
+                {
+                    __instance.m_AmountText.text = $"Stock{Environment.NewLine}{amount}";
+                }
+                else
+                {
+                    __instance.m_AmountText.text = $"Stock: 0";
+                }
             }
-            else
+            catch
             {
-                __instance.m_AmountText.text = $"Stock: 0";
+                return;
+            }
+        }
+
+        [HarmonyPatch(typeof(RestockItemScreen), "Init")]
+        [HarmonyPrefix]
+        public static void RunOnRestockLoad(RestockItemScreen __instance)
+        {
+            Plugin.Log($"Prefix for RestockItemScreen ran!");
+
+            var boxes = CGameManager.FindObjectsOfType<InteractablePackagingBox_Item>(); //find all item boxes
+            if (boxes.Length == 0)
+            {
+                Plugin.Log("No boxes found");
+                return;
+            }
+            StringBuilder sb = new();
+            try
+            {
+                sb.AppendLine($"{boxes.Length} item boxes found!");
+                StockAmounts.Clear();
+
+                foreach (var box in boxes)
+                {
+                    if (box.GetItemType() == EItemType.None) { continue; }
+                    if (!StockAmounts.TryAdd(box.GetItemType(), box.m_ItemCompartment.GetItemCount()))
+                    {
+                        StockAmounts[box.GetItemType()] += box.m_ItemCompartment.GetItemCount();
+                    }
+                }
+#if DEBUG
+                foreach (var box in StockAmounts)
+                {
+                    sb.AppendLine($"  {box.Key} : {box.Value}");
+                }
+#endif 
+
+                //StockAmounts = temp;
+                Plugin.Log(sb.ToString().Trim());
+            }
+            catch (Exception ex)
+            {
+
+                Plugin.Log(sb.ToString().Trim());
+                Plugin.Log($"error: {ex.Message}");
             }
         }
     }
--- StockCount/Plugin.cs
+++ StockCount/Plugin.cs
@@ -12,10 +12,7 @@ namespace StockCount
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        internal static new ManualLogSource Logger;
-        internal static Dictionary<EItemType, int> StockAmounts = [];
-
-        private Thread workerThread;
+        private static new ManualLogSource Logger;
 
 
         private void Awake()
@@ -24,59 +21,16 @@ namespace StockCount
             Logger = base.Logger;
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
-
-            workerThread = new Thread(new ThreadStart(StockWorker));
-            workerThread.IsBackground = true;
-            workerThread.Start();
             var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
             harmony.PatchAll();
             
         }
 
-        private void StockWorker()
+        internal static void Log(object TextToLog)
         {
-            while (true)
-            {
-                Thread.Sleep(1000);
-
-                var boxes = CGameManager.FindObjectsOfType<InteractablePackagingBox_Item>(); //find all item boxes
-                if (boxes.Length == 0)
-                {
-                    //Logger.LogInfo("No boxes found");
-                    Thread.Sleep(10000);
-                    continue;
-                }
-
-                StringBuilder sb = new();
-                try
-                {
-                    sb.AppendLine($"{boxes.Length} item boxes found!");
-                    StockAmounts.Clear();
-                    Dictionary<EItemType, int> temp = [];
-                    foreach (var box in boxes)
-                    {
-                        if (box.GetItemType() == EItemType.None) { continue; }
-                        if(!temp.TryAdd(box.GetItemType(), box.m_ItemCompartment.GetItemCount()))
-                        {
-                            temp[box.GetItemType()] += box.m_ItemCompartment.GetItemCount();
-                        }
-                    }
-
-                    foreach (var box in temp)
-                    {
-                        sb.AppendLine($"  {box.Key} : {box.Value}");
-                    }
-
-                    StockAmounts = temp;
-                    //Logger.LogInfo(sb.ToString().Trim());
-                }
-                catch (Exception ex)
-                {
-
-                    //Logger.LogInfo(sb.ToString().Trim());
-                    Logger.LogInfo($"error: {ex.Message}");
-                }
-            }
+#if DEBUG
+            Logger.LogInfo(TextToLog.ToString());
+#endif
         }
 
     }
